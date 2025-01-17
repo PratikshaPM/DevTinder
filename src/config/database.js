a7c6aeddfc61ee77652640199fdb7c101ffcafa3
PratikshaPM <pratikshamahajan2@gmail.com>
2025-01-17 13:46:09 +0530

    removed databasecred

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
-  return await mongoose.connect(
-    "mongodb+srv://mahajanpratiksha2014:qNSTWoZkohw0YTQa@cluster0.5bkys.mongodb.net/DevTinder"
-  );
+  return await mongoose.connect("xyx");
 };
 
 module.exports = connectDB;
